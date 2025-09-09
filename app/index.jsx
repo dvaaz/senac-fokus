d1ad08ad97e8ce41647477971a81a51b0d9042a7
dvaaz <darley.azevedo@gmail.com>
2025-09-09 09:50:20 -0300

    atualizacao aula 09/09/2025. Date, arow function em um map

--- app/index.jsx
+++ app/index.jsx
@@ -18,7 +18,7 @@ const pomodoro = [
     id: "long",
     initialValue: 45,
     image: require('./descanso_longo.png'),
-        display: "Pausa curta",
+        display: "Pausa longa",
   },
   
 ] 
@@ -26,26 +26,34 @@ const pomodoro = [
 
 export default function Index() {
 
-const [timerType, setTimerType] = useState(pomodoro[1]) // timerType é o Hook e setTimerType é a função
+const [timerType, setTimerType] = useState(pomodoro[0]) // timerType é o Hook e setTimerType é a função
 
   return (
     <View style={styles.container}>
       <Image source={timerType.image}/>
       <View style={styles.actions}>
-
-        <View styles={styles.context}>          
-          <Pressable style={styles.contextButtonActive}>
-            <Text style={styles.contextButtonText}>{timerType.display}</Text>
-          </Pressable>
-
+        <View style={styles.context}>          
+        { // Dentro do React chamamos o JS dentro de chaves {}
+          pomodoro.map( p=>
+            <Pressable key={p} //* um pressable utilizando o hook
+            style={ timerType.id === p.id ? styles.contextButtonActive: null}
+            onPress={() => setTimerType(p)}
+            > 
+            <Text style={styles.contextButtonText}>{p.display}</Text>
+            </Pressable>
+          )
+        }
         </View>
-
-        <Text style={styles.timer}>{timerType.initialValue}</Text>
+        <Text style={styles.timer}>
+          {/* {timerType.initialValue} */}
+          {new Date(timerType.initialValue*1000).toLocaleTimeString("pt-BR", {
+            minute: "2-digit", second: "2-digit"
+          })} {/**/}
+          </Text>
         <Pressable style={styles.button}>
           <Text style={styles.buttonText}>Começar</Text>
         </Pressable>
-
-      </View>
+        </View>
       <View style={styles.footer}>
         <Text style={styles.footerText}>
           Projeto fictício e sem fins comerciais.
@@ -65,57 +73,51 @@ const styles = StyleSheet.create({
     backgroundColor: '#021123',
     gap: 40, // só pode ser utilizado o flex ou grid já tiver sido chamado (um elemento posicionador)
   },
-  actions: {
+actions: {
     padding: 24,
     backgroundColor: '#14448080',
     borderRadius: 32,
     borderWidth: 2,
     borderColor: '#144480',
-    alignItems: "center",
     width: '80%',
-    gap: 32,
+    gap:32,
+  },
+  context:{
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-around",
+  },
+  contextButtonActive: {
+    backgroundColor: '#144480',
+    borderRadius: 8,
+  },
+  contextButtonText: {
+    color: '#FFF',
+    fontSize: 12.5,
+    padding: 8,
   },
   timer: {
     fontSize: 54,
-    color: '#FFFFFF',
+    color: '#FFF',
     fontWeight: "bold",
-    textAlign: "center"
-    
+    textAlign: "center",
   },
   button: {
-    padding: 8,
     backgroundColor: '#B872FF',
     borderRadius: 32,
+    padding: 8,
   },
   buttonText: {
-    fontSize: 18,
+    textAlign: "center",
     color: '#021123',
-    fontFamily: "Montserrat",
+    fontSize: 18,
   },
   footer: {
     width: '80%',
   },
   footerText: {
     color: '#98A0A8',
-    fontSize: 13,
-    textAlign: "center",
-  },
-  context: {
-    flexDirection: "row",
-    justifyContent: "space-around",
-    alignItems: "center",
-  },
-  contextButtonText: {
-    colo: '#FFFF',
     fontSize: 12.5,
-    padding: 8
-  },
-  contextButtonActive: {
-    backgroundColor: '#144480',
-    borderRadius: 8,
-
-  }, 
-
-
+    textAlign: "center",
+  }
 })
-  
